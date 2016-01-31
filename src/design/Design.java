09d5ef18561a3bd78c4293eed25d04b5666473f2
PC <PC@192.168.1.36>
2016-01-31 14:55:31 +0100

    first commit

---
+++ src/design/Design.java
@@ -0,0 +1,12 @@
+package design;
+
+import java.util.*;
+
+public class Design {
+
+    public static void main(String[] args) {
+        PlayerReal pr = new PlayerReal();
+
+    }
+
+}
---
+++ src/design/RealMadrid.java
@@ -0,0 +1,12 @@
+package design;
+
+public class RealMadrid {
+    String rmd = "RealMadrid";
+    public RealMadrid(){
+        
+    }
+    public RealMadrid(String rmd) {
+        this.rmd = rmd;
+    }
+    
+}
